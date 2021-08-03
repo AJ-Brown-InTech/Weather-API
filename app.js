e8a9bb60431b2cbf4d190b4d161f3e80a1401d89
AJ-Brown-InTech <75697769+AJ-Brown-InTech@users.noreply.github.com>
2021-08-03 09:47:11 -0700

    timezone, degree and description complete. working on icon

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-window.addEventListener('load', ()=>{
-  let long //making a var for long
+window.addEventListener('load', () => {
+  let long // making a var for long
   let lat // making a var for the latitude
- 
+
   /* where using querySelectors to get the dom elements in our html that we put classes on.
   I'm putting the elements into variables to make easy to call.
   */
@@ -10,32 +10,43 @@ window.addEventListener('load', ()=>{
   let timezone = document.querySelector(".timezone")
 
   if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(position => {
-  console.log(position)
-   
-  long = position.coords.longitude
-  lat = position.coords.latitude
-  const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=1c76e30a9ba9fa0559a64deef6afdc05`
+    navigator.geolocation.getCurrentPosition(position => {
+      console.log(position)
 
-  fetch(api)
-  .then(response => {
-    return response.json()
-  })
-  .then(data => {
-console.log(data)
-const {temp, description } = data.current
-// //Set DOM Elements from the API
-temp.textContent = degree
-console.log(temp)
-timezone.textContent = data.timezone
-console.log(data.timezone)
+      long = position.coords.longitude
+      lat = position.coords.latitude
+      const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=1c76e30a9ba9fa0559a64deef6afdc05`
 
-weather.textContent = description
-console.log(description)
+      fetch(api).then(response => {
+        return response.json()
+      }).then(data => {
+        //Set DOM Elements from the API
+        
+        const {temp, icon} = data.current
+        console.log(data.current)
+        degree.textContent = temp
+        console.log(temp)
+
+        timezone.textContent = data.timezone
+        console.log(data.timezone)
+
+        const avg =  data.current.weather[0].main
+        console.log(avg)
+       
+        description.textContent = avg
+        // Set Icons according to weather description
+
+        setIcons(icon, document.querySelector('.icon'))
 
 
-  })
+
+      })  
+    })
+  } 
+  function setIcons (icon,iconId){
+    const skycons = new Skycons({color: "white"})
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase()
+    skycons.play()
+    return skycons.set(iconId, Skycons[currentIcon])
+  }
 })
-}
-})
-
