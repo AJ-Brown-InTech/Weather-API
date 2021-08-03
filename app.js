c2b90b02f16b7d6060d4dbd3c7e8ac9977b5f0ff
AJ-Brown-InTech <75697769+AJ-Brown-InTech@users.noreply.github.com>
2021-08-02 21:24:05 -0700

    working through adding api data to html

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 window.addEventListener('load', ()=>{
   let long //making a var for long
   let lat // making a var for the latitude
-  let currentDate = new Date() // a var for me to keep track of my function working and having some fun with JavaScript
-
+ 
   /* where using querySelectors to get the dom elements in our html that we put classes on.
   I'm putting the elements into variables to make easy to call.
   */
@@ -13,10 +12,7 @@ window.addEventListener('load', ()=>{
   if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(position => {
   console.log(position)
-  console.log(currentDate)
-
-    
-
+   
   long = position.coords.longitude
   lat = position.coords.latitude
   const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=1c76e30a9ba9fa0559a64deef6afdc05`
@@ -27,9 +23,17 @@ window.addEventListener('load', ()=>{
   })
   .then(data => {
 console.log(data)
-// const {temperature, summary} = data.currently
+const {temp, description } = data.current
 // //Set DOM Elements from the API
-// degree.textContent = temperature
+temp.textContent = degree
+console.log(temp)
+timezone.textContent = data.timezone
+console.log(data.timezone)
+
+weather.textContent = description
+console.log(description)
+
+
   })
 })
 }
